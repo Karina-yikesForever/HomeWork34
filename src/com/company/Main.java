11fe67faaed4c67dbb8e0560b40df1dc6d5da7dc
Karina <karinakokosova8@gmail.com>
2019-12-07 13:12:35 +0600

    HW34

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<String>song=new ArrayList<>();
+        song.add("Ветер с моря дул");
+        song.add("\nНагонял беду");
+        song.add("\nИ сказал ты мне");
+        song.add("\nБольше не приду");
+        System.out.println(song.size());
+        for(int i=1;i<20;i++)
+            System.out.println(song);
+
+        }
+
     }
-}
